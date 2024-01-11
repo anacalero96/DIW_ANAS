17dcb9733b467724800a32f737f3c615dcf1e70c
anacalero96 <ana96calero@gmail.com>
2024-01-11 21:17:26 +0100

    comentarios

--- UD6/UD6.Actividad2_AnaSoledad/script.js
+++ UD6/UD6.Actividad2_AnaSoledad/script.js
@@ -4,9 +4,13 @@ $(document).ready(function(){
 
     let nRandom;
 
+    //Buscar como filtrar los números de manera que no se repita más de dos veces.
+    
     for(i = 0; i <20; i++){
         nRandom = Math.floor((Math.random()*10)+1);
         $(".cartas").append($("<div class='card' value='"+nRandom+"'>"+nRandom+"</div>"));
     };
+
+
 
 });
