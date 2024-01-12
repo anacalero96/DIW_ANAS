7cf8498841b24069efdc0143041a1be593a39263
anacalero96 <ana96calero@gmail.com>
2024-01-12 21:00:32 +0100

    actividad avanzada

---
+++ UD6/UD6.Actividad2_AnaSoledad/script.js
@@ -0,0 +1,49 @@
+//Juego de memoria
+
+$(document).ready(function(){
+
+    let nRandom;
+    let posicionRandom = new Array(20); 
+    let aux = false;        // Variable para comprobar si hay dos cartas giradas.
+    let girada;
+    let nRepeticiones = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];     //Número de veces que se repite el número.
+
+    for(i = 0; i <20;){
+        nRandom = Math.floor((Math.random()*10)+1);
+        if(nRepeticiones[nRandom -1]<2){
+            nRepeticiones[nRandom-1]++;  
+            posicionRandom[i] = nRandom;
+            i++;    //aumenta una vez añadida la carta.
+          
+            $(".cartas").append($("<div class='card' value='"+nRandom+"'>"+''+"</div>"));
+        }
+    };
+
+    $(".card").on("click", function(){
+        if($(this).text() != $(this).attr("value")){
+            $(this).text($(this).attr("value"));
+        } 
+
+        if(aux == true){
+            aux = false;
+            console.log($(this).text());
+            console.log(girada.text());
+            if($(this).text() == girada.text()){
+                $(".mensaje").text("Las cartas son iguales");
+            } else {
+                $(".mensaje").text("No coinciden las cartas");
+                setTimeout(() => { 
+                    $(this).text(""); 
+                    girada.text(""); 
+                    $(".mensaje").text("");
+                }, 1000);
+            }
+        } else {
+            aux = true;
+            girada = $(this);
+        }
+    });
+
+    //  $(this).text("");
+
+});
