1cfe891b33c30c467baebfec48cfd82b9544cbc5
anacalero96 <ana96calero@gmail.com>
2024-01-16 19:16:42 +0100

    actualizar archivos

--- UD6/UD6.Actividad2_AnaSoledad/script.js
+++ UD6/UD6.Actividad2_AnaSoledad/script.js
@@ -3,14 +3,43 @@
 $(document).ready(function(){
 
     let nRandom;
+    let posicionRandom = new Array(20); 
+    let aux = false;        //Variable para comprobar si hay dos cartas giradas.
+    let girada;
+    let nRepeticiones = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];     //Número de veces que se repite el número.
 
-    //Buscar como filtrar los números de manera que no se repita más de dos veces.
-    
-    for(i = 0; i <20; i++){
+    for(i = 0; i <20;){
         nRandom = Math.floor((Math.random()*10)+1);
-        $(".cartas").append($("<div class='card' value='"+nRandom+"'>"+nRandom+"</div>"));
+        if(nRepeticiones[nRandom -1]<2){
+            nRepeticiones[nRandom-1]++;  
+            posicionRandom[i] = nRandom;
+            i++;    //Aumenta una vez añadida la carta.
+          
+            $(".cartas").append($("<div class='card' value='"+nRandom+"'>"+'&nbsp;'+"</div>"));
+        }
     };
 
+    $(".card").on("click", function(){
+        if($(this).text()!= $(this).attr("value")){
+            $(this).text($(this).attr("value"));
+        } 
 
+        if(aux == true){
+            aux = false;
+            if($(this).text() == girada.text()){
+                $(".mensaje").text("Las cartas son iguales");
+            } else {
+                $(".mensaje").text("No coinciden las cartas");
+                setTimeout(() => { 
+                    $(this).html("&nbsp"); 
+                    girada.html("&nbsp"); 
+                    $(".mensaje").text("");
+                }, 1000);
+            }
+        } else {
+            aux = true;
+            girada = $(this);
+        }
+    });
 
 });
