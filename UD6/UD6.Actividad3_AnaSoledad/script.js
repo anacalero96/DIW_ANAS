c9474babe6bb38280e914b1627d940d8e6c28a56
anacalero96 <ana96calero@gmail.com>
2024-01-19 16:25:21 +0100

    Ejercicio completado

--- UD6/UD6.Actividad3_AnaSoledad/script.js
+++ UD6/UD6.Actividad3_AnaSoledad/script.js
@@ -4,22 +4,36 @@ $(document).ready(function(){
     $("textarea").countCharacters();
 });
 
-//https://api.jquery.com/keyup/#on-
 
 $.fn.countCharacters = function(){
     // this.css("background", "pink");
     // return this;
 
-    var texto1 = $("textarea").val();   
-    var texto2 = texto1.length;
+    var texto1 = $("#text1").val().length;   //Representa el primer textarea
+    $("#text1").after("<p> <span id='paco'></span> </p>");
 
-    $("form").data("test", texto2);
-
-    $("span").html($("form").data("test")+ "characters");
+    $("form").data("test", texto1); 
+    $("#paco").html($("form").data("test")+ " characters");
 
     $("#text1").keyup(function(){
-
+        texto1 = $("#text1").val().length; 
+        $("form").data("test", texto1); 
+        $("#paco").html($("form").data("test")+ " characters");
     });
 
 
+    var texto2 = $("#text2").val().length;
+    $("#text2").after("<p> <span id='paco2'></span> </p>" );
+
+    $("form").data("test2", texto2); 
+    $("#paco2").html($("form").data("test2")+ " characters");
+  
+
+    $("#text2").keyup(function(){ 
+        texto2 = $("#text2").val().length; 
+        $("form").data("test2", texto2);
+        $("#paco2").html($("form").data("test2")+ " characters");
+    });
 };
+
+//He usado el nombre de paco en las dos 'id' porque es un nombre recurrente a la hora de hacer pruebas!
