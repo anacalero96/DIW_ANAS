ecdc3130003e199052e103393a450873a286c6ab
anacalero96 <ana96calero@gmail.com>
2024-01-24 17:11:51 +0100

    modificar funciones

--- UD6/UD6.Actividad4_AnaSoledad/script.js
+++ UD6/UD6.Actividad4_AnaSoledad/script.js
@@ -1,15 +1,30 @@
 //https://jqueryui.com/draggable/
 
+//Contadores para recoger la cantidad post-it que hay en cada tablero.
+var contadorVioleta = 0;
+var contadorTurquesa = 0;
+
+
+
+
 $(document).ready(function(){
     $( ".draggable_violeta, .draggable_turquesa" ).draggable();
 
     //contenedor1 es el que contiene los post-it de color lila oscuro.
     $("#contenedor1").droppable({
         accept: ".draggable_violeta",       //solo acepta los post-it color violeta/lila
-        classes: {
-            "ui-droppable-active": "ui-state-active",
-            "ui-droppable-hover": "ui-state-hover"
+        drop:function(ui){
+            violetaId = ui.draggable.prop("id");
+            console.log(violetaId);
+            if($("div#" + violetaId).hasClass("purple")){
+                console.log("droppable");
+            } else{
+                contadorVioleta++;
+                $("div#"+ violetaId).removeClass("purple");
+                $("contenedor1").find("p").html("Hay tantos post-it"+ violetaId);
+            }
         }
+
     });
     //contenedor2 contiene los post-it de color azul.
     $("#contenedor2").droppable({
