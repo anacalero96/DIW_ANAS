ca43cdaac2492be181622326bea248f1ae60737f
anacalero96 <ana96calero@gmail.com>
2024-01-24 16:34:03 +0100

    cambios en los archivos

--- UD6/UD6.Actividad4_AnaSoledad/script.js
+++ UD6/UD6.Actividad4_AnaSoledad/script.js
@@ -1,5 +1,27 @@
 //https://jqueryui.com/draggable/
 
-$( function() {
-    $( "#draggable" ).draggable();
+$(document).ready(function(){
+    $( ".draggable_violeta, .draggable_turquesa" ).draggable();
+
+    //contenedor1 es el que contiene los post-it de color lila oscuro.
+    $("#contenedor1").droppable({
+        accept: ".draggable_violeta",       //solo acepta los post-it color violeta/lila
+        classes: {
+            "ui-droppable-active": "ui-state-active",
+            "ui-droppable-hover": "ui-state-hover"
+        }
+    });
+    //contenedor2 contiene los post-it de color azul.
+    $("#contenedor2").droppable({
+        accept: ".draggable_turquesa",
+        classes: {
+            "ui-droppable-active": "ui-state-active",
+            "ui-droppable-hover": "ui-state-hover"
+        } 
+    });
 });
+
+
+
+
+ 
