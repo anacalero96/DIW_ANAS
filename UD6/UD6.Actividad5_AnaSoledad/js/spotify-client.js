0a6bcae7a6b6944872a8e275fa1aab458c4fbb09
anacalero96 <ana96calero@gmail.com>
2024-02-06 23:34:56 +0100

    Actividad finalizada

--- UD6/UD6.Actividad5_AnaSoledad/js/spotify-client.js
+++ UD6/UD6.Actividad5_AnaSoledad/js/spotify-client.js
@@ -7,29 +7,41 @@ function Spotify() {
   this.apiUrl = 'https://api.spotify.com/';
 }
 
-var spotify = new Spotify();
+//El $.ajax está comentado porque no me funciona, anteriormente si consegui que fuera, pero ahora solo
+//me deja consultar los datos utilizando las variables de arriba y poniendo las credenciales ''.
 
 // $.ajax({
 //   url: './js/keys.json',
 //   dataType: 'json',
 //   success: function (data) {
 //     client_id = data.client_id;
-//     client_secret = data.client_secret;
+//     client_secret = data.client_secret;  
 //   }
 // });
+
+var spotify = new Spotify();
+
+$.ajax({
+  url: './js/keys.json',
+  dataType: 'json',
+  success: function (data) {
+    client_id = data.client_id;
+    client_secret = data.client_secret;  
+  }
+});
 
 //Search for information on an artist, adding the possibility of obtaining their albums.
 Spotify.prototype.getArtist = function (artist) {
   $.ajax({
     type: "GET",
-    url: this.apiUrl + 'v1/search?type=artist&q=' + artist,
+    url: this.apiUrl + 'v1/search?type=artist,track&q=' + artist,
     headers: {
       'Authorization' : 'Bearer ' + access_token
     },
   }).done( function(response){
     $("#results").empty("");
+    $("#tracks").empty("");
     // console.log(response);
-
     let items = response.artists.items;
 
     items.map(function(item){
@@ -45,24 +57,39 @@ Spotify.prototype.getArtist = function (artist) {
       let src = item.images.length !=0? item.images[0].url :'No_Image_Available.jpg';
 
       $("#results").append(`
-      <div>
-        <h3>${name}</h3>
-        <h4>Popularity: ${popularity}</h4>
-        <a><img src='${src}'></a>
-      </div>
+        <div>
+          <h3>${name}</h3>
+          <h4>Popularity: ${popularity}</h4>
+          <img id="${id}" src='${src}'>
+        </div>
       `); 
     
-      $(document).on("click", function(){
+      $("#" + id).on("click", function(){
         $("#results").empty("");
+        $("#tracks").empty("");
         spotify.getArtistById(id);
       });
-    });
+    });  
+
+    let trackItems = response.tracks.items;
+
+    trackItems.map(function(item){
+      let id = item.id;
+      let name = item.name;   //Variable to obtain the artist.
+      
+      $("#tracks").append(`
+        <div>
+          <h3>${name}</h3>
+          <img id="${id}" src=''>
+        </div>
+      `); 
+      spotify.getTracksById(id);
+    });  
   });
 };
 
 //Search the albums of an artist, given the id of the artist
 Spotify.prototype.getArtistById = function (artistId) {
-
   $.ajax({
     type: "GET",
     url: this.apiUrl + 'v1/artists/' + artistId + '/albums',
@@ -74,26 +101,26 @@ Spotify.prototype.getArtistById = function (artistId) {
 
     items.map(function(item){
       let id = item.id;
-      // let urlSpotify = item.external_urls;
       let name = item.name;
-
       let src = item.images.length !=0? item.images[0].url :'No_Image_Available.jpg';
 
       $("#results").append(`
-        <div id='${id}'>
+        <div id="${id}">
           <h3>${name}</h3>
           <img src='${src}'>
         </div>
       `);
       
+      //When you click on an album, you get the songs.
       $("#" + id + ">img").on("click", function(){
+        $("#tracks").empty("");
         spotify.getTracks(id);
       });
     });
   });
 };
 
-//Muestra las canciones en los albumes seleccionados.
+//Displays the songs in the selected albums.
 Spotify.prototype.getTracks = function (albumName) { 
   $.ajax({
     type: "GET",
@@ -102,49 +129,40 @@ Spotify.prototype.getTracks = function (albumName) {
       'Authorization' : 'Bearer ' + access_token
     },
   }).done( function(response){
-    $("#" + albumName + ">div").remove("");
     
     let items = response.items;
-    // let src = item.images.length !=0? item.images[0].url :'No_Image_Available.jpg';
 
+  //Gets values and inserts them into the html using append.
       items.map(function(item) {
-        let name = item.name; 
-        $("#" + albumName).append(`
-        <div>
-          <h3>${name}</h3>
-        </div>
-        `);
+        let name = item.name;  
+        let id = item.id;
+        $("#tracks").append(`
+          <div>
+            <h3>${name}</h3>
+            <img id="${id}" src=''>
+          </div>
+        `); 
+        spotify.getTracksById(id);
       });
   });
 };
 
-Spotify.prototype.getSongs = function (songName) { 
+//Gets the url of the album image that belongs to a song and allows you to add an image when searching.
+Spotify.prototype.getTracksById = function (trackId) {
   $.ajax({
     type: "GET",
-    url: this.apiUrl + 'v1/search?type=artists,track&q=' + songName,    
+    url: this.apiUrl + 'v1/tracks/' + trackId ,
     headers: {
       'Authorization' : 'Bearer ' + access_token
     },
   }).done( function(response){
-    $("#results").empty("");
-    let items = response.items;
+    console.log(response);
 
-      items.map(function(item) {
-        let urlSpotify = item.external_urls.spotify;
-        let name = item.name; 
-        let src = item.images.length !=0? item.images[0].url :'No_Image_Available.jpg';
-
-        $("#results").append(`
-        <div>
-          <h3>${name}</h3>
-          <a href='${urlSpotify}'><img src='${src}'></a>
-        </div>
-        `);
-      });
+    let src = response.album.images.length !=0? response.album.images[0].url :'No_Image_Available.jpg';
+  
+    $("#" + trackId).attr("src", src);
   });
-};
-
-
+}
 
 //This fragment is the first thing that is loaded, when the $(document).ready
 $(function () {
@@ -160,20 +178,13 @@ $(function () {
     access_token = response.access_token;    
   });
 
-  // var spotify = new Spotify();
-
   $('#bgetArtist').on('click', function () {
     // console.log($('#artistName').val());
     spotify.getArtist($('#artistName').val());
   });
 
   $('#results').on('click', '.artistId', function () {
+    // $("#results").empty("");
     spotify.getArtistById($(this).attr("data-id"));
   });
-
-  $('#getSongs').on('click', function () {
-    $("#albums").empty("");
-    spotify.getSongs($("#").val());
-  });
-
 });
