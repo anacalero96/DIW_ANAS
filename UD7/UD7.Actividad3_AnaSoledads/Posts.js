1c57b32dbfc6ccc58155ebd43aa8893f6599403a
anacalero96 <ana96calero@gmail.com>
2024-02-21 17:19:06 +0100

    Componentes con Vue

--- UD7/UD7.Actividad3_AnaSoledads/Posts.js
+++ UD7/UD7.Actividad3_AnaSoledads/Posts.js
@@ -1,9 +1,14 @@
 export default {
     name: "Posts",
-    props:["list"],
+    props:["item"],
+    methods: {
+         
+    },
     template: `
+        <h1 class="title-listposts">LIST OF POSTS</h1>
         <p>{{item.title}} {{item.resum}} {{item.autor}} {{item.img}} {{item.fecha}}</p>
-            <img v-bind:src="item.img">
-            <button id="btn_delete" type="button" @click="deletePost(item)">DELETE</button>
-            <button id="btn_edit" type="button" @click="editPost(item)">EDIT</button> `,
+        <img v-bind:src="item.img">
+        <button id="btn_delete" type="button" @click="deletePost(item)">DELETE</button>
+        <button id="btn_edit" type="button" @click="editPost(item)">EDIT</button>
+     `,
 }
