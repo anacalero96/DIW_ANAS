a9e6a6532029103ef21e3f1c09f7ee68a24ffb5e
anacalero96 <ana96calero@gmail.com>
2024-02-22 01:15:24 +0100

    Nuevo componente Posts

--- UD7/UD7.Actividad3_AnaSoledads/Posts.js
+++ UD7/UD7.Actividad3_AnaSoledads/Posts.js
@@ -1,14 +1,20 @@
 export default {
     name: "Posts",
-    props:["item"],
+    props:["item"]
+,
     methods: {
-         
+        delete_post: function(item){
+            console.log("entrado");
+            this.$emit("delete_post", item);
+        },
+        edit_post:function(item){
+            this.$emit("edit_post", item);
+        }
     },
     template: `
-        <h1 class="title-listposts">LIST OF POSTS</h1>
         <p>{{item.title}} {{item.resum}} {{item.autor}} {{item.img}} {{item.fecha}}</p>
         <img v-bind:src="item.img">
-        <button id="btn_delete" type="button" @click="deletePost(item)">DELETE</button>
-        <button id="btn_edit" type="button" @click="editPost(item)">EDIT</button>
-     `,
+        <button id="btn_delete" type="button" @click="delete_post(item)">DELETE</button>
+        <button id="btn_edit" type="button" @click="edit_post(item)">EDIT</button>
+  `,
 }
