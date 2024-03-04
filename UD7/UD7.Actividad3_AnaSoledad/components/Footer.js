b84652998199ac0f7e3fb0b4d040fe6b88586da1
anacalero96 <ana96calero@gmail.com>
2024-03-04 21:15:40 +0100

    Modificaciones de css

--- UD7/UD7.Actividad3_AnaSoledad/components/Footer.js
+++ UD7/UD7.Actividad3_AnaSoledad/components/Footer.js
@@ -2,12 +2,16 @@
 export default {
     name: "New_footer",
     template: `
-    <ul class="nav col-6 col-sm-6 ">
+    <ul class="nav col-10 col-sm-10 justify-content-center">
         <li class="nav-item"><a href="#" class="nav-link px-3 text-light">HOME</a></li>
         <li class="nav-item"><a href="#" class="nav-link px-3 text-light">MENU</a></li>
         <li class="nav-item"><a href="#" class="nav-link px-3 text-light">SOCIAL MEDIA</a></li>
         <li class="nav-item"><a href="#" class="nav-link px-3 text-light">PRIVACY POLICY</a></li>
         <li class="nav-item"><a href="#" class="nav-link px-3 text-light">CONTACT US</a></li>
-        <p class=" col-6 col-sm-6 text-light text-end">© 2024 Ana.S Martínez Calero</p>
-    </ul> `,
+        <p class=" col-2 col-sm-2 text-light text-end">© 2024 Ana.S Martínez Calero</p>
+    </ul> 
+
+      
+   
+    `,
 }
